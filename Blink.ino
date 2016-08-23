d911d063987393bb37d7787587f3a458dfeeda5b
Tiago Mergulhão <me@tmergulhao.com>
2016-08-22 21:02:10 -0500

    Blink.ino: better timing functions and modular variables

--- Blink.ino
+++ Blink.ino
@@ -6,9 +6,6 @@ int gaugerPort = A5;
 
 // LOGICS
 
-int buttonInput = LOW, persistentButtonInput = LOW;
-bool state = false;
-
 void setup() {
   pinMode(outputPort, OUTPUT);
   pinMode(buttonPort,INPUT);
@@ -18,6 +15,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  static int persistentButtonInput = LOW;
+  static bool state = false;
   int gaugerInput = analogRead(gaugerPort);
   int buttonInput = digitalRead(buttonPort);
 
@@ -28,30 +27,33 @@ void loop() {
 
   persistentButtonInput = buttonInput;
 
-  // digitalWrite(outputPort, state ? HIGH : LOW);
-  // analogWrite(outputPort, state ? 255 * sensorValue / 1023 : 0); // 0 .. 255
-  fadeTo(state, gaugerInput);
+  if (itsTime(10)) { // in milliseconds
+    if (state) {
+      fadeTo(gaugerInput);
+    } else {
+      fadeTo(0);
+    }
+  }
 
-  delay(5); // wait 1/4 of a secong
 }
 
-//int analogOutput = 0;
+bool itsTime (long int interval) {
+  static unsigned long previousTime = 0;
+  unsigned long time = millis();
+  if (time - previousTime > interval) {
+    previousTime = time;
+    return true;
+  }
+  return false;
+}
 
-void fadeTo (int state, int gaugerInput) {
+void fadeTo (int gaugerInput) {
   static int analogOutput = 0;
 
-  int errorMargin = analogOutput - gaugerInput;
-
-  if (state) {
-  	if (errorMargin > 0) {
-      analogOutput--;
-    } else if (errorMargin < 0) {
-      analogOutput++;
-    }
-  } else {
-    if (analogOutput > 0) {
-      analogOutput--;
-    }
+  if (analogOutput - gaugerInput > 0) {
+    analogOutput--;
+  } else if (analogOutput - gaugerInput < 0) {
+    analogOutput++;
   }
 
   analogWrite(outputPort, analogOutput * 255 / 1023);
